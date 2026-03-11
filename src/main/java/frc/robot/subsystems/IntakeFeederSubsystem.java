a34095f53682d07957184f1c8b81042e612777b6
Spacatsu <29charles.lee@gmail.com>
2026-03-11 11:00:47 -0500

    blah

--- src/main/java/frc/robot/subsystems/IntakeFeederSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeFeederSubsystem.java
@@ -9,6 +9,9 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.*;
 
@@ -34,12 +37,54 @@ public class IntakeFeederSubsystem extends SubsystemBase {
 
   private SparkMax indexMotor = new SparkMax(MotorIDs.kIndexer, MotorType.kBrushless);
 
+  private NetworkTable nt = NetworkTableInstance.getDefault().getTable("Shooter");
+
+  private NetworkTableEntry intakeFlopperPositionEntry = nt.getEntry("Intake Flopper Position"),
+      intakeFlopperGoalEntry = nt.getEntry("Intake Flopper Goal"),
+      intakeFlopperkp = nt.getEntry("intakeFlopperkp"), // TODO doesn't need to be here
+      intakeFlopperki = nt.getEntry("intakeFlopperki"), // TODO doesn't need to be here
+      intakeFlopperkd = nt.getEntry("intakeFlopperkd"), // TODO doesn't need to be here
+      intakeFlopperMaxSpeed = nt.getEntry("intakeFlopperMaxSpeed"), // TODO doesn't need to be here
+      intakeFlopperacel = nt.getEntry("intakeFlopperacel"), // TODO doesn't need to be here
+      indexerSpeedEntry = nt.getEntry("Indexer Speed"),
+      indexerGoalEntry = nt.getEntry("Indexer Goal"),
+      indexerkp = nt.getEntry("indexerkp"), // TODO doesn't need to be here
+      indexerki = nt.getEntry("indexerki"), // TODO doesn't need to be here
+      indexerkd = nt.getEntry("indexerkd"), // TODO doesn't need to be here
+      indexerMaxSpeed = nt.getEntry("indexerMaxSpeed"), // TODO doesn't need to be here
+      indexeracel = nt.getEntry("indexeracel"); // TODO doesn't need to be here
+
+
   public IntakeFeederSubsystem() {
+    intakeFlopperkp.setDefaultDouble(0);
+    intakeFlopperki.setDefaultDouble(0);
+    intakeFlopperkd.setDefaultDouble(0);
+    intakeFlopperMaxSpeed.setDefaultDouble(0);
+    intakeFlopperacel.setDefaultDouble(0);
+    indexerkp.setDefaultDouble(0);
+    indexerki.setDefaultDouble(0);
+    indexerkd.setDefaultDouble(0);
+    indexerMaxSpeed.setDefaultDouble(0);
+    indexeracel.setDefaultDouble(0);
+
     flopperPID.setGoal(0);
   }
 
   @Override
   public void periodic() {
+    flopperPID.setPID(intakeFlopperkp.getDouble(0),
+        intakeFlopperki.getDouble(0),
+        intakeFlopperkd.getDouble(0));
+    flopperPID.setConstraints(new TrapezoidProfile.Constraints(intakeFlopperMaxSpeed.getDouble(0), intakeFlopperacel.getDouble(0)));
+
+    indexerPID.setPID(indexerkp.getDouble(0), indexerki.getDouble(0), indexerkd.getDouble(0));
+    indexerPID.setConstraints(new TrapezoidProfile.Constraints(indexerMaxSpeed.getDouble(0), indexeracel.getDouble(0)));
+
+    intakeFlopperPositionEntry.setDouble(IntakeFlopperMotor.getEncoder().getPosition());
+    intakeFlopperGoalEntry.setDouble(flopperPID.getSetpoint().position);
+    indexerGoalEntry.setDouble(indexerPID.getSetpoint().position);
+    indexerSpeedEntry.setDouble(indexMotor.getEncoder().getVelocity());
+
     IntakeFlopperMotor.set(flopperPID.calculate(IntakeFlopperMotor.getEncoder().getPosition()));
     indexMotor.set(indexerPID.calculate(indexMotor.getEncoder().getVelocity()));
   }
